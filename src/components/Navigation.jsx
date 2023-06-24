628fbeb3dc24de07ad6551f189f5f834ad0196cc
elhousing <emmanuelrub_@hotmail.com>
2023-06-24 00:26:53 -0600

    Terminado el header

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,45 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import pokemonLogo from "../assets/Pokemon_logo.svg"
 
 const Navigation = () => {
   return (
-    <div>
-      <Outlet/>
-    </div>
-  )
-}
+    <>
+      <header className="flex justify-between px-20 h-24 items-center max-md:flex-col max-md:gap-5">
+        <Link>
+          <img src={pokemonLogo} alt="Pokemon Logo" className="w-44"/>
+        </Link>
+        <form action="" className="flex justify-between  w-[500px] gap-2 items-center max-md:flex-col max-md:w-80 max-md:gap-5">
+          <div className="flex items-center rounded-full border-2 border-sky-600 pr-4 py-1 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 mx-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+
+            <input 
+            className="w-72 h-9 pl-2"
+            type="search"
+            name="valueSearch"
+            id=""
+            placeholder="Buscar nombre de pokemon"
+            // value={valueSearch}
+            // onChange={onInputChange}
+            />
+          </div>
+          <button className="block bg-yellow-400 px-6 h-10 rounded-full text-blue-600 font-bold">Buscar</button>
+        </form>
+      </header>
+      <Outlet />
+    </>
+  );
+};
 export default Navigation;
